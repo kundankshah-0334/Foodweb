157a2e78791b4251a99575bf0bfb6cf58eae3bb0
kundan kumar sahu <kundanlal96580@gmail.com>
2023-09-25 23:41:01 +0530

    db connected

--- server/dbs/db.js
+++ server/dbs/db.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose')
 const URL = "mongodb+srv://kundanlal96580:CLiyooZVYjX0fqa3@cluster0.5pd6vv9.mongodb.net/FoodOrder?retryWrites=true&w=majority";
 
 const mongoDB = async () => {
-    await mongoose.connect(URL , ()=>{
-        console.log('Connected');
-    })
+    try {
+        mongoose.connect(URL);
+        console.log('db connected');
+    } catch (error) {
+        console.log('db not connected',error);
+    }
+   
 }
 
 module.exports = mongoDB;
--- server/index.js
+++ server/index.js
@@ -1,31 +1,10 @@
 // import mongoose from "mongoose";
 const mongoose = require('mongoose')
-// const mongoDB = require("./dbs/db")
+const mongoDB = require("./dbs/db")
 // import  express  from "express";
 const express = require("express")
 const PORT = 8000;
 const app = express();
-
-
-// const conn =  mongoose.connect("mongodb+srv://kundanlal96580:CLiyooZVYjX0fqa3@cluster0.5pd6vv9.mongodb.net/FoodOrder?retryWrites=true&w=majority");
-
-const mongoDB = async () => {
-   await mongoose.connect("mongodb+srv://kundanlal96580:CLiyooZVYjX0fqa3@cluster0.5pd6vv9.mongodb.net/FoodOrder?retryWrites=true&w=majority");
-   // Handle connection events
-const db = mongoose.connection;
-console.log(db)
-db.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-db.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
-
-db.on('disconnected', () => {
-  console.log('MongoDB disconnected');
-});
-}
 
 mongoDB();
 // console.log(conn);
