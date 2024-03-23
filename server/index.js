1cf1b01b7db695f4b94544d8983ea7dc6312a294
kundan kumar sahu <kundanlal96580@gmail.com>
2024-03-23 20:06:35 +0530

    20   06

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoDB = require("./dbs/db")
 const cors = require('cors');
 require("dotenv").config();
 const express = require("express")
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
